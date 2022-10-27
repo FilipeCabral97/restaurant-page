daf384f4b70d9c5e61c985593d3b8459c1278043
Filipe Cabral <rui.filipe.cabral.97@gmail.com>
2022-10-27 16:23:28 +0100

    Create home layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("JUST A TEST");
+import "./style.css";
+
+const component = () => {
+  const backgroundImage = new Image();
+  backgroundImage.src = Wallpaper;
+};
+
+// document.body.appendChild(backgroundImage)
